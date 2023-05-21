3249a6a1fe177be5e06a4599ccf640ca9e0d976a
Rambo31 <slava-goodkov@mail.ru>
2023-05-21 12:19:19 +0300

    Saving data: dummy death counter works per character

--- doItInYourPants.lua
+++ doItInYourPants.lua
@@ -5,7 +5,19 @@ end
 
 local function OnEvent()
 	if event == 'ADDON_LOADED' and arg1 == 'doItInYourPants' then
-		print('Hello, world! - doItInYourPants')
+		if DoItInYourPantsCharVault == nil then
+			DoItInYourPantsCharVault = 0
+			print('Greetings traveler! doItInYourPants addon has been activated!')
+		elseif DoItInYourPantsCharVault == 0 then
+			print('Greetings traveler! You have never been dead, congratulations!')
+			DoItInYourPantsCharVault = DoItInYourPantsCharVault + 1
+		elseif DoItInYourPantsCharVault == 1 then 
+			print('Welcome stranger! You died once!')
+			DoItInYourPantsCharVault = DoItInYourPantsCharVault + 1
+		else
+			print('Welcome stranger! You died ' .. DoItInYourPantsCharVault .. ' times!')
+			DoItInYourPantsCharVault = DoItInYourPantsCharVault + 1
+		end
 	end
 end
 doItInYourPants = CreateFrame('Frame', 'doItInYourPants', UIParent)
